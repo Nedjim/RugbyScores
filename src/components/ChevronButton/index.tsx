1232435b1a36d34890ab8ac316f0eb37d3919d61
Nedjim <nedjim.danimon@gmail.com>
2025-08-08 14:47:24 +0200

    update theme

--- src/components/ChevronButton/index.tsx
+++ src/components/ChevronButton/index.tsx
@@ -3,6 +3,7 @@ import { memo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import IconButton from "@mui/material/IconButton";
+import styles from "./index.module.scss";
 
 type Props = {
   direction: "left" | "right";
@@ -13,7 +14,12 @@ function ChevronButton({ direction, onClick }: Props) {
   const icon = direction === "left" ? faAngleLeft : faAngleRight;
 
   return (
-    <IconButton onClick={onClick} size="small" color="inherit">
+    <IconButton
+      onClick={onClick}
+      size="small"
+      color="inherit"
+      className={styles.chevronButton}
+    >
       <FontAwesomeIcon icon={icon} />
     </IconButton>
   );
